a4769dc4c762d0423a6f2417f893bf6bc0af91ff
sangho235 <sangho235@it.gsshop.com>
2021-07-15 16:13:24 +0900

    스토어 만들기

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
 import './index.css';
 import App from './App';
+import rootReducer from './modules';
 import reportWebVitals from './reportWebVitals';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
