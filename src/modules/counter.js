999ee651afe3a795201e97d36998afb76024e694
sangho235 <sangho235@it.gsshop.com>
2021-07-16 23:16:46 +0900

    redux action 적용

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,29 +1,25 @@
+//redux-actions적용
+import {createAction, handleActions} from 'redux-actions';
+
 /*액션 타입 정의 하기*/
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
 //액션 생성 함수
-export const increase = () => ({type : INCREASE});
-export const decrease = () => ({type : DECREASE});
+export const increase = createAction(INCREASE); //redux-actions 적용
+export const decrease = createAction(DECREASE); //redux-actions 적용
 
 //초기 상태 및 리듀서 함수 만들기
 const initialState = {
 	number : 0
 };
 
-function counter(state = initialState, action) {
-	switch(action.type) {
-		case INCREASE :
-			return {
-				number : state.number + 1
-			};
-		case DECREASE :
-			return {
-				number : state.number - 1
-			};
-		default :
-			return state;
-	}
-}
+const counter = handleActions(
+	{
+		[INCREASE]: (state, action) => ({number: state.number + 1}),
+		[DECREASE]: (state, action) => ({number: state.number - 1}),
+	},
+	initialState,
+);
 
 export default counter;
